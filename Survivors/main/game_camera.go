618634c16840c99b68ff478f724d36b33fc5ed6d
Raymond McDonald <raymond@raymondmcdonald.net>
2024-11-14 15:13:58 -0600

    Fiddle with tilemap and setup game camera

---
+++ Survivors/main/game_camera.go
@@ -0,0 +1,13 @@
+components {
+  id: "game_camera"
+  component: "/main/game_camera.script"
+}
+embedded_components {
+  id: "camera"
+  type: "camera"
+  data: "aspect_ratio: 1.0\n"
+  "fov: 0.7854\n"
+  "near_z: 0.1\n"
+  "far_z: 1000.0\n"
+  ""
+}
