4bbfba93dc0e9b1260f22f5b0a7095f8ffd79471
Raymond McDonald <raymond@raymondmcdonald.net>
2024-11-14 16:56:52 -0600

    Set up basic enemy and implement basic movement
    The enemy follows the player forever and it is tightly coupled to exactly one player. The player also posts a message to the exactly one enemy about its position. This needs to be architected for many enemies to many players, and I will learn to do that in time with the message passing system and after implementing enemy factories.

---
+++ Survivors/enemies/basic_enemy.go
@@ -0,0 +1,41 @@
+components {
+  id: "basic_enemy"
+  component: "/enemies/basic_enemy.script"
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"enemies\"\n"
+  "mask: \"players\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 7.3239436\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"basic_enemy\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/sprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: 1.0
+  }
+}
