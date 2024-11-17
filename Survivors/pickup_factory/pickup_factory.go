9779b05b7c27728dbd390a6e634734e634bc2729
Raymond McDonald <raymond@raymondmcdonald.net>
2024-11-16 18:33:15 -0600

    Implement basic experience drop on enemy death

---
+++ Survivors/pickup_factory/pickup_factory.go
@@ -0,0 +1,10 @@
+components {
+  id: "pickup_factory"
+  component: "/pickup_factory/pickup_factory.script"
+}
+embedded_components {
+  id: "experience_vial_factory"
+  type: "factory"
+  data: "prototype: \"/experience_vial/experience_vial.go\"\n"
+  ""
+}
