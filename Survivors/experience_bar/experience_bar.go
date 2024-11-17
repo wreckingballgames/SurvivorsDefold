d12617f416741469d224ad82ac9cd00229601be7
Raymond McDonald <raymond@raymondmcdonald.net>
2024-11-16 19:26:52 -0600

    Implement experience bar

---
+++ Survivors/experience_bar/experience_bar.go
@@ -0,0 +1,8 @@
+components {
+  id: "experience_bar"
+  component: "/experience_bar/experience_bar.gui"
+}
+components {
+  id: "experience_bar_script"
+  component: "/experience_bar/experience_bar.script"
+}
